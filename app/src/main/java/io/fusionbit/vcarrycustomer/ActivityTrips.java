9ccfb6728cb00fd963beae7f4b84bf02502c76b6
rutvik <rutvik106@live.com>
2017-05-16 15:11:54 +0530

    @Home, Corrected This Month trips not showing properly.

--- app/src/main/java/io/fusionbit/vcarrycustomer/ActivityTrips.java
+++ app/src/main/java/io/fusionbit/vcarrycustomer/ActivityTrips.java
@@ -190,7 +190,8 @@ public class ActivityTrips extends BaseActivity
                 };
 
         final String today = Utils.getDate(Calendar.getInstance().getTime());
-        final Date month = Utils.addDays(Calendar.getInstance().getTime(), -30);
+        final Date month = Utils.addDays(Calendar.getInstance().getTime(),
+                -1 * (Calendar.getInstance().get(Calendar.DAY_OF_MONTH) - 1));
         final String monthInString = Utils.getDate(month);
 
         api.getTripSummary(customerId, tripStatus, monthInString, today, null,
